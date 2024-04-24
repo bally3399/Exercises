ba7028e872eba723ab2e7192b0fb83c687112c10
DELL <sulaimabaliqis@gmail.com>
2024-04-24 13:27:26 +0100

    exercise

--- src/exercise/Exercise1.java
+++ src/exercise/Exercise1.java
@@ -1,2 +1,18 @@
-package exercise;public class Exercise1 {
+package exercise;
+
+public class Exercise1 {
+    public static String isPalindrome(String letter) {
+        boolean isPalindrome = true;
+        for (int index = 0; index < letter.length() / 2; index++) {
+            if (letter.charAt(index) != letter.charAt(letter.length() - 1 - index)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome) {
+            return "Yes";
+        }else {
+            return "No";
+        }
+    }
 }
--- test/exercise/Exercise1Test.java
+++ test/exercise/Exercise1Test.java
@@ -1,4 +1,29 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class Exercise1Test {
-  
+
+public class Exercise1Test {
+    @Test
+    public void testPalindrome() {
+        String letter = "madam";
+        String expected = "Yes";
+        String result = Exercise1.isPalindrome(letter);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testPalindrome1() {
+        String letter = "Bally";
+        String expected = "No";
+        String result = Exercise1.isPalindrome(letter);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testPalindrome2() {
+        String letter = "o";
+        String expected = "Yes";
+        String result = Exercise1.isPalindrome(letter);
+        assertEquals(expected, result);
+    }
 }
